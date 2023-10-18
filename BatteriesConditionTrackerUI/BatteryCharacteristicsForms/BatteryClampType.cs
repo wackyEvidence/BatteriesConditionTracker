88f56d598ee0251d2c8f36cb88cb9b48c15140e3
wackyEvidence <nizapogiti@gmail.com>
2023-10-18 22:08:06 +0500

    Reorganized UI projects files into corresponding folders, renamed the UI project

---
+++ BatteriesConditionTrackerUI/BatteryCharacteristicsForms/BatteryClampType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatteriesConditionTrackerUI
+{
+    public partial class BatteryClampType : Form
+    {
+        public BatteryClampType()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
